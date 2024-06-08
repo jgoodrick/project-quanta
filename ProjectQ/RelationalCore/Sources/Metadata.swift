c52ed019186603e3a018d71dde35c28ec179c6c7
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2024-06-07 20:03:29 -0500

    Made the Metadata and Tracked types internal to the RelationalCore module

--- ProjectQ/RelationalCore/Sources/Metadata.swift
+++ ProjectQ/RelationalCore/Sources/Metadata.swift
@@ -1,30 +1,26 @@
 
 import Foundation
 
-public struct Metadata: Hashable, Codable, Sendable {
+struct Metadata: Hashable, Codable, Sendable {
     init(now: Date) {
         self.added = now
         self.modified = now
     }
     
-    public var added: Date
-    public var modified: Date
+    var added: Date
+    var modified: Date
 }
 
 extension Metadata: Mergeable {
-    public mutating func merge(with incoming: Metadata) {
+    mutating func merge(with incoming: Metadata) {
         added = min(added, incoming.added)
         modified = max(modified, incoming.modified)
     }
 }
 
-//@dynamicMemberLookup
-public struct Tracked<Value> {
-    public var value: Value
-    public var metadata: Metadata
-//    public subscript<T>(dynamicMember keyPath: KeyPath<Value, T>) -> T {
-//      self.value[keyPath: keyPath]
-//    }
+struct Tracked<Value> {
+    var value: Value
+    var metadata: Metadata
 }
 
 extension Tracked: Mergeable where Value: Mergeable {
@@ -40,10 +36,10 @@ extension Tracked: Decodable where Value: Decodable {}
 extension Tracked: Sendable where Value: Sendable {}
 extension Tracked: Hashable where Value: Hashable {}
 extension Tracked: Identifiable where Value: Identifiable {
-    public var id: Value.ID { value.id }
+    var id: Value.ID { value.id }
 }
 extension Tracked {
-    public init(value: Value, now: Date) {
+    init(value: Value, now: Date) {
         self.init(value: value, metadata: .init(now: now))
     }
 }
