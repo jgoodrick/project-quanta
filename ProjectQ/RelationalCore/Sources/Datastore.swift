a69a7f2590b36d959ba4ddde416385a5eae93dd8
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2024-06-07 20:09:49 -0500

    moved relationship set to its own file

--- ProjectQ/RelationalCore/Sources/Datastore.swift
+++ ProjectQ/RelationalCore/Sources/Datastore.swift
@@ -26,18 +26,3 @@ public struct Database: Equatable, Codable, Sendable {
     }
     
 }
-
-protocol RelationshipSet: Equatable, Mergeable {
-    init()
-}
-
-extension RelationshipSet {
-    var isOrphan: Bool { self == .init() }
-}
-
-extension Dictionary where Value: RelationshipSet {
-    subscript(id id: Key) -> Value {
-        get { self[id, default: .init()] }
-        set { self[id] = newValue }
-    }
-}
