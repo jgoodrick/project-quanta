93c0eb3c1161fb7e12ae4526000196dc7ef8aac2
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2025-02-16 12:55:10 -0600

    starting from scratch with SharingGRDB

---
+++ ProjectQDB/ProjectQDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectQDBApp.swift
+//  ProjectQDB
+//
+//  Created by Goodrick,Joseph on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectQDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectQDBTests/ProjectQDBTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectQDBTests.swift
+//  ProjectQDBTests
+//
+//  Created by Goodrick,Joseph on 2/16/25.
+//
+
+import Testing
+
+struct ProjectQDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
