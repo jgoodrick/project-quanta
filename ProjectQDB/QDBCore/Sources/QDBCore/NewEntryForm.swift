37d6a9ed02e0b3d095066fc9bfeea35537561f27
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2025-07-04 21:43:47 -0500

    wip: before removing DB.Entry.Spelling

--- ProjectQDB/QDBCore/Sources/QDBCore/NewEntryForm.swift
+++ ProjectQDB/QDBCore/Sources/QDBCore/NewEntryForm.swift
@@ -12,14 +12,25 @@ import SwiftUI
 struct NewEntryForm: View {
     @State private var word = ""
 
+    @FetchAll var spellingMatches: [SpellingMatch]
+
     @State private var languageId: String = .currentLanguageId()
 
     @FetchAll var availableLanguageNames: [DB.Language.Name]
 
-    @State private var matchDBValue = false
+    @State private var selectedTranslationLanguageIds: [DB.Language.Name.ID] = {
+        @Shared(.lastSelectedTranslationLanguageId) var lastSelected
+        return [lastSelected]
+    }()
+
+    @Dependency(\.defaultDatabase) private var db
 
     init(word: String = "") {
         self._word = .init(initialValue: word)
+    }
+
+    var selectedTranslations: [DB.Language.Name] {
+        availableLanguageNames.filter { selectedTranslationLanguageIds.contains($0.code) }
     }
 
     var body: some View {
@@ -33,23 +44,65 @@ struct NewEntryForm: View {
                     .pickerStyle(.navigationLink)
             }
 
-            ForEach(availableLanguageNames) { languageName in
-                if languageName.id != languageId {
-                    Section(languageName.text) {
-                        SubEntryForm(title: languageName.text)
+            if !spellingMatches.isEmpty {
+//                ForEach(selectedTranslations.indices, id: \.self) { index in
+//                    let language = selectedTranslations[index]
+//                    if language.id != languageId {
+//                        ProposedTranslation(
+//                            language: language,
+//                            text: 
+//                        )
+//                    }
+//                }
+            }
+        }
+        .toolbar {
+            if spellingMatches.isEmpty {
+                Button("Save") {
+                    withErrorReporting {
+                        try db.write { db in
+                        }
                     }
                 }
             }
         }
-        .toolbar {
-            if !matchDBValue {
-                Button("Save") {
-
-                }
-            }
+        .task(id: word) {
+            try? await updateQuery()
         }
-        .navigationTitle("New Word")
+        .navigationTitle(spellingMatches.isEmpty ? "Add" : "Update")
     }
+
+
+    @Selection
+    struct SpellingMatch: Hashable, Identifiable {
+        var id: DB.Entry.ID { entry.id }
+        var entry: DB.Entry
+        var exact: Bool
+    }
+
+    private func updateQuery() async throws {
+        try await $spellingMatches.load(
+            DB.Entry
+                .group(by: \.id)
+                .join(DB.Language.Name.all) { $0.language.eq($1.code) }
+                .where { $1.primaryKey.eq(languageId) }
+                .join(DB.Entry.Spelling.all) { $0.spelling.eq($2.id) }
+                .where { _, _, spelling in
+                    spelling.text.collate(.nocase).like(word)
+                }
+                .order { entry, languageName, spelling in
+                    (languageName.code.eq(languageId), entry.recorded)
+                }
+                .select { entry, _, _ in
+                    SpellingMatch.Columns.init(
+                        entry: entry,
+                        exact: false
+                    )
+                }
+                .distinct(true)
+        )
+    }
+
 }
 
 extension String {
@@ -59,25 +112,21 @@ extension String {
     }
 }
 
-struct SubEntryForm: View {
-    let title: String
-    @State private var text = ""
-    @State private var languageId = ""
+struct ProposedTranslation: View {
+    let language: DB.Language.Name
+    @Binding var text: String
 
     var body: some View {
-//        VStack {
-            TextField(title, text: $text)
+        Section(language.text) {
+            TextField(language.text, text: $text)
                 .font(.largeTitle)
                 .textFieldStyle(.roundedBorder)
-//
-//            LanguagePicker(languageId: $languageId)
-//                .pickerStyle(.navigationLink)
-//        }
+        }
     }
 }
 
 #Preview { let _ = DB.prepare()
     NavigationStack {
-        NewEntryForm()
+        NewEntryForm(word: "hello")
     }
 }
--- ProjectQDB/QDBCore/Sources/QDBCore/Shared.swift
+++ ProjectQDB/QDBCore/Sources/QDBCore/Shared.swift
@@ -14,6 +14,13 @@ extension SharedKey where Self == AppStorageKey<String>.Default {
         @Dependency(\.locale) var locale
         return Self[.appStorage("languageId"), default: locale.language.languageCode?.identifier ?? "en"]
     }
+
+    static var lastSelectedTranslationLanguageId: Self {
+        @Dependency(\.defaultDatabase) var db
+        @Shared(.languageId) var current
+        let available = try? db.read { db in try DB.Language.Name.all.order(by: \.code).fetchAll(db) }
+        return Self[.appStorage("lastSelectedTranslationLanguageId_from_\(current)"), default: available?.first?.code ?? "en"]
+    }
 }
 
 extension SharedKey where Self == InMemoryKey<String>.Default {
